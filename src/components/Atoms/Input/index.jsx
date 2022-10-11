b2dac10ff27c34c98dc002f79911be15730b300d
SakaueYae <y.sakaue@4nonome.com>
2022-10-11 17:10:14 +0900

    大まかなレイアウト作成

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const Sample = () => {
+  useEffect(() => {
+    console.log("initialized");
+  }, []);
+
+  return null;
+};
+export default Sample;
